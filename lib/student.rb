cc53dcd93cb9fb1df5b586a3d54c3d4043649e8a
James Rogers <jarogers095@gmail.com>
2020-03-22 08:55:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
   attr_reader(:id, :name, :grade)
   
-  def initialize(id = nil, name, grade)
+  def initialize(name, grade, id = nil)
     @id = id
     @name = name
     @grade = grade
